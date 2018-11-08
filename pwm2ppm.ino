42eff18528628d0740c9ed4ee53e93eb82fa4c37
PZotya <39873307+PZotya@users.noreply.github.com>
2018-11-08 23:17:47 +0100

    Update pwm2ppm.ino

--- pwm2ppm.ino
+++ pwm2ppm.ino
@@ -7,7 +7,7 @@ struct channel{
 
 volatile channel channelList[PPMMAXCHANNELNUM];
 volatile int chnum;
-//volatile int PPMOut Fix PPM out to PIN 2
+
 volatile unsigned int currentLength;
 
 volatile unsigned long inputChange;
@@ -21,7 +21,7 @@ volatile byte lastPinStateD;
 ISR(PCINT0_vect){
   byte currPins,changeMask;
   inputChange=micros();
-  currPins=PINB&((1<<PCINT0)|(1<<PCINT1));
+  currPins=PINB&((1<<PCINT0)|(1<<PCINT1));//currPins=PINB&((1<<PCINT0)|(1<<PCINT1)|(1<<PCINT2)|(1<<PCINT3));
   changeMask=lastPinStateB^currPins;
   if( changeMask & (1<<PCINT0) )
   {    
@@ -48,10 +48,10 @@ ISR(PCINT0_vect){
   if( changeMask & (1<<PCINT2) )
   {    
     if ( currPins & (1<<PCINT2) ) {
-      channelList[2].lastChange=inputChange;
+      channelList[6].lastChange=inputChange;
     } else  {
-      if (channelList[2].lastChange !=0) {
-        channelList[2].value=(inputChange-channelList[2].lastChange)*2;
+      if (channelList[6].lastChange !=0) {
+        channelList[6].value=(inputChange-channelList[6].lastChange)*2;
       }
     }
   }
@@ -59,35 +59,14 @@ ISR(PCINT0_vect){
   if( changeMask & (1<<PCINT3) )
   {    
     if ( currPins & (1<<PCINT3) ) {
-      channelList[3].lastChange=inputChange;
+      channelList[7].lastChange=inputChange;
     } else  {
-      if (channelList[3].lastChange !=0) {
-        channelList[3].value=(inputChange-channelList[3].lastChange)*2;
+      if (channelList[7].lastChange !=0) {
+        channelList[7].value=(inputChange-channelList[7].lastChange)*2;
       }
     }  
   }
-
-  if( changeMask & (1<<PCINT4) )
-  {    
-    if ( currPins & (1<<PCINT4) ) {
-      channelList[4].lastChange=inputChange;
-    } else  {
-      if (channelList[4].lastChange !=0) {
-        channelList[4].value=(inputChange-channelList[4].lastChange)*2;
-      }
-    }
-  }
-  
-  if( changeMask & (1<<PCINT5) )
-  {    
-    if ( currPins & (1<<PCINT5) ) {
-      channelList[5].lastChange=inputChange;
-    } else  {
-      if (channelList[5].lastChange !=0) {
-        channelList[5].value=(inputChange-channelList[5].lastChange)*2;
-      }
-    }
-  }     
+ 
 */
   
    lastPinStateB=currPins;
@@ -144,32 +123,7 @@ ISR(PCINT2_vect){
         channelList[3].value=(inputChange-channelList[3].lastChange)*2;
       }
     }  
-  }
-
-/*
-  if( changeMask & (1<<PCINT4) )
-  {    
-    if ( currPins & (1<<PCINT4) ) {
-      channelList[4].lastChange=inputChange;
-    } else  {
-      if (channelList[4].lastChange !=0) {
-        channelList[4].value=(inputChange-channelList[4].lastChange)*2;
-      }
-    }
-  }
-  
-  if( changeMask & (1<<PCINT5) )
-  {    
-    if ( currPins & (1<<PCINT5) ) {
-      channelList[5].lastChange=inputChange;
-    } else  {
-      if (channelList[5].lastChange !=0) {
-        channelList[5].value=(inputChange-channelList[5].lastChange)*2;
-      }
-    }
-  }     
-*/
-  
+  }  
    lastPinStateD=currPins;
  } 
 
@@ -181,7 +135,6 @@ ISR(TIMER1_COMPA_vect) // timer compareA interrupt service routine
 {
   PORTD |= (1<<PCINT18);
   TCNT1  = 0;
-//  digitalWrite(PPMOut, HIGH);
   chnum++; if (chnum > PPMMAXCHANNELNUM) { chnum=0;}
   if (chnum ==PPMMAXCHANNELNUM){
         OCR1A = 45000-currentLength;
@@ -196,7 +149,6 @@ ISR(TIMER1_COMPA_vect) // timer compareA interrupt service routine
 ISR(TIMER1_COMPB_vect) // timer compareB interrupt service routine
 {
   PORTD &= ~(1<<PCINT18);
-//    digitalWrite(PPMOut, LOW);
 }
 
 void setChannel(int channel, int pin, unsigned int initValue) {
@@ -217,7 +169,13 @@ void setup() {
   setChannel(6,9,1500); //aux2
   setChannel(7,0,1000); //RSSI or any ADC
   setChannel(8,0,1000); // FailSafe
-//  PPMOut=2;
+ /*
+  setChannel(7,10,1500); 
+  setChannel(8,11,1500);
+  setChannel(9,0,1000); //RSSI or any ADC
+  setChannel(10,0,1000); // FailSafe  
+  */
+  
   pinMode(2,OUTPUT);
 
 //  digitalWrite(PPMOut, HIGH);
@@ -233,22 +191,20 @@ void setup() {
   currentLength = 0;
   TCCR1B |= (1 << WGM12);   // CTC mode
   TCCR1B |= (1 << CS11);    // 8 prescaler 
-//  TCCR1A = B11110000;   
+
   TIMSK1 |= (1 << OCIE1B )|( 1 << OCIE1A);  // enable timer compare interrupt
 
   PCICR |= (1<<PCIE0)|(1<<PCIE2);
-  PCMSK0 |= (1<<PCINT0)|(1<<PCINT1);
+  PCMSK0 |= (1<<PCINT0)|(1<<PCINT1); //  PCMSK0 |= (1<<PCINT0)|(1<<PCINT1)|(1<<PCINT2)|(1<<PCINT3);
   PCMSK2 |= (1<<PCINT20)|(1<<PCINT21)|(1<<PCINT22)|(1<<PCINT23);
 
   
-  lastPinStateB= PINB&(1<<PCINT0)|(1<<PCINT1);
+  lastPinStateB= PINB&(1<<PCINT0)|(1<<PCINT1);//lastPinStateB= PINB&(1<<PCINT0)|(1<<PCINT1);|(1<<PCINT2)|(1<<PCINT3);
   lastPinStateD= PIND&(1<<PCINT20)|(1<<PCINT21)|(1<<PCINT22)|(1<<PCINT23);
   
   inputChange=micros();  
   
   interrupts();             // enable all interrupts
-
-//  PORTD |= (1<<PCINT18);
 
 
 }
@@ -260,36 +216,14 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-int i;
-int chPin;
-
 
 if (( micros()- inputChange)>200000)
 {
-  channelList[7].value=4000;
+  channelList[7].value=4000; //  channelList[9].value=4000;
 } else {
-  channelList[7].value=2000;    
+  channelList[7].value=2000;   //   channelList[9].value=2000; 
 }
 
-channelList[6].value=map(analogRead(0),0,1023,2000,4000 );
-/*
-for(i=0;i<PPMMAXCHANNELNUM;i++) {
-  if (channelList[i].pin != 0){
-  chPin=digitalRead(channelList[i].pin);
-  if ((chPin==LOW) && !channelList[i].clearInput) {
-    channelList[i].clearInput=true;
-    } else {    
-      if (chPin!=channelList[i].state){
-          channelList[i].state=chPin;
-          if (chPin==LOW){
-              channelList[i].value=(micros()-channelList[i].lastChange)*2;
+channelList[6].value=map(analogRead(0),0,1023,2000,4000 ); // channelList[8].value=map(analogRead(0),0,1023,2000,4000 );
 
-            } else {
-              channelList[i].lastChange=micros();
-            }
-          }
-        }
-    }
-}
-*/
 }
